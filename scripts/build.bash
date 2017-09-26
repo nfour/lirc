823d40912501e3163a3a842009156a6b339cb5bc
nfour <novus.nfour@gmail.com>
2017-09-26 21:43:07 +1000

    fix build

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,5 +1,11 @@
+rm -rf build
+mkdir build
+
 # Build
 coffee -c -o build src
 
 # Copy over root files
 rsync -am . ./build  --exclude '*/*' --include '*'
+
+# Copy latent files from source, recursively
+rsync -am ./src/* ./build --exclude '*.coffee'
